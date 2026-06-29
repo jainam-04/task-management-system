42b4e2e62263fff6480b8b9f106a5b3ca0b86cf6
Jainam Rupani <jainamrupani04@gmail.com>
2026-06-29 13:45:03 +0530

    Login frontend done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='container'>
+      <p className='logo'>Tasky</p>
+      <div className="sub-container">
+            <Link to="/" className='nav-items'>Home</Link>
+            <Link to="/about" className='nav-items'>About</Link>
+            <Link to="/contact" className='nav-items'>Contact</Link>
+            <Link to="/login" className='nav-items'>Login</Link>
+      </div>
     </div>
   )
 }
